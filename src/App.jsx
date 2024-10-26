0b3732ed16c885de87e26529445e9aa51d9ad9bd
gurjarshraddhapatel <gurjarshraddha100@gmail.com>
2024-10-27 04:37:03 +0530

    get product

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Category from './component/admin/Category';
 import Demo from './component/admin/demo';
 import Product from './component/admin/Product';
 import VariantForm from './component/admin/VariantForm';
-import ProductsList from './component/admin/ProductsList';
+// import ProductsList from './component/admin/ProductsList';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route path="/postdata" element={<Product/>} />
         <Route path="/category" element={<Category/>} />
         <Route path="/varients" element={<VariantForm/>} />
-        <Route path="/prodictlist" element={<ProductsList/>} />
+        {/* <Route path="/prodictlist" element={<ProductsList/>} /> */}
         <Route path="/demo" element={<Demo/>} />
       </Routes>
     </Router>
